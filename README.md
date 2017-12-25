eaf74c7e16f57b432ba8ccc566df10047a3d6453
大兵哥哥 <285274947@qq.com>
2017-12-26 00:17:59 +0800

    update shortcut

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TabLayout
 Android实现TabLayout
+
+![image](https://github.com/luweibin3118/TabLayout/app/Screenshot_20171226-001548.png)
--- app/src/main/java/com/example/administrator/tablayout/MainActivity.java
+++ app/src/main/java/com/example/administrator/tablayout/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-
 import com.example.library.TabLayout;
 
 import java.util.ArrayList;
